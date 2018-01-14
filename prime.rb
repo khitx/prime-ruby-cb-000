cca48eded6a46aa139ba8b1208a6419a2e13c74c
Kathy Hudson <khitx@icloud.com>
2018-01-14 20:41:56 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+def prime?(int)
+  i = 2
+  if int == 2
+    true
+  end
+  sqrtInt = int.sqrt
+  while i <= sqrtInt
+    if int % int == 0
+      false
+    end
+    i += 1
+  end
+  true
+end
